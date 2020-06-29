fab4cfb09251f7ade872b13ef1435f41fda20819
thiagowfer <thiagowfer@gmail.com>
2020-06-29 20:07:45 -0300

    Bootstrap table added

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <title>CRUD PHP JS</title>
 </head>
@@ -17,5 +18,10 @@
         });
     });
 </script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js">
+</script>
+
 </body>
 </html>
--- listar_usuarios.php
+++ listar_usuarios.php
@@ -3,16 +3,33 @@ include_once "config/conexao.php";
 
 $sql = "SELECT * FROM usuarios ORDER BY id DESC";
 $resultado = mysqli_query($conn, $sql);
-
-if($resultado && $resultado->num_rows != 0){
-    while($row_usuario = mysqli_fetch_assoc($resultado)){
-        echo "Nome: "  . ucwords($row_usuario['nome']). "<br>";
-        echo "Email: " . ucwords($row_usuario['email']). "<br>";
-        echo "<hr>";
-    }
-} 
-else {
-    echo "<h2>Nenhum usuário encontrado!</h2>";
-}
-
 ?>
+
+<table class="table table-bordered table-striped">
+    <thead>
+        <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php 
+if($resultado && $resultado->num_rows != 0):
+    while($row_usuario = mysqli_fetch_assoc($resultado)):
+?>
+
+    <tr>
+        <th> <?php echo $row_usuario['id'];    ?></th>
+        <td> <?php echo ucwords($row_usuario['name']);  ?></td>
+        <td> <?php echo $row_usuario['email']; ?></td>
+    </tr>
+
+<?php 
+    endwhile;
+endif; 
+?>
+
+</tbody>
+</table>

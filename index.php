52f7fac0d280be2d6b1613c977e0970b22606d89
thiagowfer <thiagowfer@gmail.com>
2020-07-02 10:21:55 -0300

    Pagination finished

--- index.php
+++ index.php
@@ -1,42 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<title>CRUD PHP JS</title>
-</head>
-<body>
-<div class="container">
-    <h2 class="mt-4 mb-4">Listar Usuários</h2>
-    <span id="conteudo"></span>
-</div>   
-
-<script>
-    var pagina = 1; // página inicial
-    var qtd_result_pagina = 10; // quantidade de registro por página
-    
-    $(document).ready(function(){
-        listarUsuario(pagina, qtd_result_pagina);
-    });
-
-    function listarUsuario(pagina, qtd_result_pagina){
-        var dados = {
-            pagina: pagina,
-            qtd_result_pagina: qtd_result_pagina
-        }
-
-        $.post('listar_usuarios.php', function(retorna){
-            $("#conteudo").html(retorna);
-        });
-    }
-
-</script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js">
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js">
-</script>
-
-</body>
+<!DOCTYPE HTML>
+<html lang="pt-br">  
+    <head>  
+        <meta charset="utf-8">
+        <title>Celke - Listar com JavaScript</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+		<div class="container">
+			<h2>Listar Usuários</h2>
+			<span id="conteudo"></span>
+		</div>
+		
+		<script>
+			var qnt_result_pg = 50; //quantidade de registro por página
+			var pagina = 1; //página inicial
+			$(document).ready(function () {
+				listar_usuario(pagina, qnt_result_pg); //Chamar a função para listar os registros
+			});
+			
+			function listar_usuario(pagina, qnt_result_pg){
+				var dados = {
+					pagina: pagina,
+					qnt_result_pg: qnt_result_pg
+				}
+				$.post('listar_usuario.php', dados , function(retorna){
+					//Subtitui o valor no seletor id="conteudo"
+					$("#conteudo").html(retorna);
+				});
+			}
+		</script>
+    </body>
 </html>
